1548499ad79f33c62b3b529ac151381f5b5942e9
YSH <yang5864@naver.com>
2026-04-03 14:18:38 +0900

    260403 실버3 DP

---
+++ BOJ/Solution_17626.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+public class Solution_17626 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
