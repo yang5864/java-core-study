9f9185a208378b55e176b1cce0ea4459ed180701
YSH <yang5864@naver.com>
2026-04-20 15:20:08 +0900

    260420 실버2 BFS

---
+++ BOJ/Solution_11724.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution_11724 {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M, u, v;
+
+    static void dfs(int node) {
+        visited[node] = true;
+        for (Integer i : graph[node]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+
+        int count = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
